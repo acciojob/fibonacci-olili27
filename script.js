22b3d12e955d83629ddf1f037f7b9512655b21eb
Timothy Olili <timmolili27@gmail.com>
2024-06-18 19:56:20 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	if(num == 0) {
+		return 0;
+	}
+
+	if(num ==1) {
+		return 1;
+	}
+
+	return (num - 1) + (num - 2);
 }
 
 module.exports = fibonacci;
